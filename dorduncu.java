0b498e88bfc9643f1db313af7b706b8555e3e922
ismail <113896260+77ismail@users.noreply.github.com>
2022-09-19 21:28:55 +0300

    Add files via upload

---
+++ dorduncu.java
@@ -0,0 +1,19 @@
+
+package eskiler;
+
+public class dorduncu {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+    }
+}
+    }
+}
+            package eskiler;
+
+public class dorduncu {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+    }
+}
